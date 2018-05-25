d20071f80413f77ac5ceda91ae7d8693192594ec
gino <gino@emq.com>
2018-05-25 16:12:38 +0800

    no message

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get('/webhook', function(req, res) {
 
 app.post('/webhook', function (req, res) {
   var data = req.body;
-
+  console.log("hahahahaha webhook");
   // Make sure this is a page subscription
   if (data.object == 'page') {
     // Iterate over each entry
